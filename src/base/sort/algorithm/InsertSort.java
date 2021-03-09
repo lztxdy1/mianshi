8f845f1cd4622c3b9b67e671cd116cf00732100d
supreme <980237501@qq.com>
2021-03-09 17:07:05 +0800

    Initial commit

--- src/base/sort/algorithm/InsertSort.java
+++ src/base/sort/algorithm/InsertSort.java
@@ -7,7 +7,7 @@ public class InsertSort {
     //对数组中的元素排序  插入排序
     public static void insertSort(Comparable[] a){
         for (int i = 1; i < a.length; i++) {
-            //当前元素为a[i]，依次和前面的元素比较，找打一个小于等于a[i]的元素
+            //当前元素为a[i]，依次和前面的元素比较，找到一个小于等于a[i]的元素
             for (int j = i; j > 0; j--)
                 if (greater(a[j - 1], a[j])) {
                     //交换元素
